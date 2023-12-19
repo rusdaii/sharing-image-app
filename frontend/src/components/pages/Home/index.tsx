a93f3dadc885cb2bc018ea932812465991b39716
rusdaii <rusdaii.html@gmail.com>
2023-12-19 18:29:53 +0700

    add loading while fetch images data

--- frontend/src/components/pages/Home/index.tsx
+++ frontend/src/components/pages/Home/index.tsx
@@ -1,22 +1,39 @@
 'use client';
+import { useEffect, useState } from 'react';
+
 import FileForm from '@/components/parts/FileForm';
 import ImageCard from '@/components/parts/ImageCard';
 import { useImages } from '@/query/images';
 import { ImageProps } from '@/repositories/images/types';
 
 const Home = () => {
+  const [isLoading, setLoading] = useState(true);
+
   const { data } = useImages();
 
   const images: ImageProps[] = data?.data ?? [];
 
+  useEffect(() => {
+    if (data) {
+      setLoading(false);
+    }
+  }, [data]);
+
   return (
     <div className="h-screen p-16">
       <FileForm />
-      <div className="grid grid-cols-3 gap-4">
-        {images?.map((item: ImageProps) => (
-          <ImageCard key={item.id} item={item} />
-        ))}
-      </div>
+
+      {isLoading ? (
+        <div className="flex justify-center items-center h-1/2">
+          <span className="loading loading-dots loading-lg" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images?.map((item: ImageProps) => (
+            <ImageCard key={item.id} item={item} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
